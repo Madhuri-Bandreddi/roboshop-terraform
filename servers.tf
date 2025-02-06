0526020fba40cd841584f131000892abea458876
mammu <bandreddymammu599@gmail.com>
2025-02-06 19:56:34 +0530

    terraform

---
+++ servers.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "centos" {
+  owners = ["703671922613"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "frontend"
+
+  }
+}
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z06377673P2QZ3HGG0TOY"
+  name    = "frontend.madhari123.shop"
+  type    = "A"
+  tt1     = 30
+  records = [aws_instance.frontend.private_ip]
+}
