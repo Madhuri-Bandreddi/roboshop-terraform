9f4f518096b75dd78fd22b1c75385719235b15c9
mammu <bandreddymammu599@gmail.com>
2025-02-06 20:52:46 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,27 +1,51 @@
+# data "aws_ami" "centos" {
+#  owners      = ["703671922613"]
+ # most_recent = true
+  #name_regex  = "Centos-8-DevOps-Practice"
+#}
+#
+#variable "instance_type" {
+ # default = "t2.micro"
+#}
+
+#resource "aws_instance" "frontend" {
+ # ami           = data.aws_ami.centos.image_id
+ # instance_type = var.instance_type
+
+#  tags = {
+ #   Name = "frontend"
+ # }
+#}
+
+#resource "aws_route53_record" "frontend" {
+ # zone_id = "Z06377673P2QZ3HGG0TOY"
+  #name    = "frontend.madhari123.shop"
+ # type    = "A"
+  #ttl     = 30
+  #records = [aws_instance.frontend.private_ip]
+#}
+
 data "aws_ami" "centos" {
-  owners      = ["703671922613"]
+  owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default ="t2.micro"
 }
 
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-
   tags = {
     Name = "frontend"
   }
 }
-
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z06377673P2QZ3HGG0TOY"
-  name    = "frontend.madhari123.shop"
-  type    = "A"
-  ttl     = 30
+  zone_id  = "Z07633651VJKTEQ867N3J"
+  name     = "frontend-dev.rdevopsb72.online"
+  type     = "A"
+  ttl      = 30
   records = [aws_instance.frontend.private_ip]
 }
-
