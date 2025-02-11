f9c9351d32fe7cbb741ec8a74455c1f3383d4f5d
mammu <bandreddymammu599@gmail.com>
2025-02-11 16:51:17 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,20 +1,31 @@
-resource "aws_instance" "instance" {
-  for_each               = var.components
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
-  # vpc_security_group_ids = [ data.aws_security_group.launch-wizard-16.id ]
-  vpc_security_group_ids = [ data.aws_security_group.allow_all1.id ]
+module "servers" {
+  for_each       =  var.components
 
-  tags = {
-    Name = each.value["name"]
-  }
+  source         =  "./module"
+  component_name =  each.value["name"]
+  env            =  var.env
+  instance_type  =  each.value["instance_type"]
+  password       =  lookup(each.value, "password", "null")
 }
 
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id  = "Z06377673P2QZ3HGG0TOY"
-  name     = "${each.value["name"]}-dev.rdevopsb72.online"
-  type     = "A"
-  ttl      = 30
-  records  = [aws_instance.instance[each.value["name"]].private_ip]
-}
+
+# resource "aws_instance" "instance" {
+#   for_each               = var.components
+#   ami                    = data.aws_ami.centos.image_id
+#   instance_type          = each.value["instance_type"]
+#   # vpc_security_group_ids = [ data.aws_security_group.launch-wizard-16.id ]
+#   vpc_security_group_ids = [ data.aws_security_group.allow_all1.id ]
+#
+#   tags = {
+#     Name = each.value["name"]
+#   }
+# }
+#
+# resource "aws_route53_record" "records" {
+#   for_each = var.components
+#   zone_id  = "Z06377673P2QZ3HGG0TOY"
+#   name     = "${each.value["name"]}-dev.rdevopsb72.online"
+#   type     = "A"
+#   ttl      = 30
+#   records  = [aws_instance.instance[each.value["name"]].private_ip]
+# }
