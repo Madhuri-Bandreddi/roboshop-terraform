c8df7c77f19230a8ca47489f5c28da4117d001f8
mammu <bandreddymammu599@gmail.com>
2025-02-12 21:40:46 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
+  for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
+  instance_type          = each,value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.default VPC security group.id]
 
   tags = {
